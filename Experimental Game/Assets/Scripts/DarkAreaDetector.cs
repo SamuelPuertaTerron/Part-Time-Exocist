97fdb865812b37e2dc570e6dd09d366a37095adb
JayK1tsune <jdforrest22@gmail.com>
2022-12-14 15:58:17 +0000

    testing light check and spawner

---
+++ Experimental Game/Assets/Scripts/DarkAreaDetector.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class DarkAreaDetector : MonoBehaviour
+{
+    // A reference to the AR session's light estimation component
+    public ARSessionOrigin sessionOrigin;
+
+    // The minimum ambient light intensity that we consider "dark"
+    public float darkThreshold = 0.15f;
+
+    void Update()
+    {
+        // Get the current light estimation data from the AR session
+        LightEstimationData lightData = sessionOrigin.GetComponent<LightEstimation>().lightEstimation;
+
+        // Check if the ambient intensity is below the dark threshold
+        if (lightData.averageBrightness < darkThreshold)
+        {
+            // The environment is considered "dark"
+            Debug.Log("It's dark in here!");
+        }
+    }
+}

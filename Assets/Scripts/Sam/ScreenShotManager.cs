63aefe5c66b4bd8510771fcead0da6bf09a47037
Meathook563 <78488173+Meathook563@users.noreply.github.com>
2023-04-05 15:52:32 +0100

    Added the Gallery

--- Assets/Scripts/Sam/ScreenShotManager.cs
+++ Assets/Scripts/Sam/ScreenShotManager.cs
@@ -3,33 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-namespace PartTimeExocist {
-    public static class ScreenShotManager {
+namespace PartTimeExocist
+{
+    public static class ScreenShotManager
+    {
 
         public static string FolderName { get { return m_folderName; } }
 
-        private static string m_folderName = Application.persistentDataPath + @"\Screenshots\";
+        private static string m_folderName = Application.persistentDataPath;
         private static int m_index = 1;
 
-        public static void TakeScreenShot(string name) {
-            if (Application.isFocused) {
-                if (!System.IO.Directory.Exists(m_folderName)) {
-                    Directory.CreateDirectory(m_folderName);
-                    if (File.Exists(m_folderName + name + ".png")) {
-                        ScreenCapture.CaptureScreenshot(m_folderName + name + m_index + ".png");
-                        m_index++;
-                    } else {
-                        ScreenCapture.CaptureScreenshot(m_folderName + name + ".png");
-                    }
-                } else {
-                    if (File.Exists(m_folderName + name + ".png")) {
-                        ScreenCapture.CaptureScreenshot(m_folderName + name + m_index + ".png");
-                        m_index++;
-                    } else {
-                        ScreenCapture.CaptureScreenshot(m_folderName + name + ".png");
-                    }
-                }
-            }
+        public static void TakeScreenShot(string name)
+        {
+            
+            ScreenCapture.CaptureScreenshot(name + m_index + ".png");
+            m_index++;            
+
+            // if (!Directory.Exists(m_folderName))
+            // {
+            //     Directory.CreateDirectory(m_folderName);
+            //     
+            // }
+            // else
+            // {
+            //     if (File.Exists(m_folderName + name + ".png"))
+            //     {
+            //         ScreenCapture.CaptureScreenshot(m_folderName + name + m_index + ".png");
+            //         m_index++;
+            //     }
+            //     else
+            //     {
+            //         ScreenCapture.CaptureScreenshot(m_folderName + name + ".png");
+            //     }
+            // }
         }
     }
 }
--- Assets/Scripts/Sam/UIPaloriodCreator.cs
+++ Assets/Scripts/Sam/UIPaloriodCreator.cs
@@ -5,25 +5,25 @@ using System.IO;
 
 namespace PartTimeExocist {
     public class UIPaloriodCreator : MonoBehaviour {
-        [SerializeField] private Transform parent;
-        [SerializeField] private GameObject Image;
-
+        [SerializeField] private Transform pictureParent;
+        [SerializeField] private Transform ropeParent;
+        [SerializeField] private GameObject image;
+        [SerializeField] private GameObject rope;
         private void Start() {
             CreatePhotos();
         }
 
         public void CreatePhotos() {
-
             if (Directory.Exists(ScreenShotManager.FolderName)) {
                 if (Directory.GetFiles(ScreenShotManager.FolderName).Length > 0) {
-                    
                     foreach(string file in Directory.GetFiles(ScreenShotManager.FolderName)) {
                         Texture2D texture = new Texture2D(500, 500);
                         byte[] fileBye = File.ReadAllBytes(file);
 
                         texture.LoadImage(fileBye);
-                        GameObject obj = Instantiate(Image, parent);
-                        obj.GetComponent<UnityEngine.UI.RawImage>().texture = texture;
+                        GameObject pictureObj = Instantiate(image, pictureParent);
+                        pictureObj.GetComponentInChildren<UnityEngine.UI.RawImage>().texture = texture;
+                        GameObject ropeObj = Instantiate(rope, ropeParent);
                     }
                 }
             }

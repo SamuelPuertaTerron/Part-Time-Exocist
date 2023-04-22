a17425419369df825b04368923bed1750efaf477
Jay <91475458+JayK1tsune@users.noreply.github.com>
2023-04-23 00:06:45 +0100

    Added Prefab spawner
    Edited nathan's code slightly  to improve menu, other bits to be added later.

--- Assets/Scripts/Jason/GameManager.cs
+++ Assets/Scripts/Jason/GameManager.cs
@@ -16,16 +16,4 @@ public class GameManager : MonoBehaviour
             Destroy(gameManager);
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 }
--- Assets/Scripts/Nathan/OptionsMenu.cs
+++ Assets/Scripts/Nathan/OptionsMenu.cs
@@ -6,10 +6,18 @@ public class OptionsMenu : MonoBehaviour
 {
    
     public Animator animator;
+    public GameObject mainMenuButtons;
 
    public void Change(bool Open)
     {
         animator.SetBool("Open", Open);
         animator.SetBool("CanPlay", true);
+        mainMenuButtons.SetActive(false);
+    }
+    //added a new function for using your code as a base for the back button, simple fix. Jay 
+    public void BackButton(bool Open){
+        animator.SetBool("Open", Open);
+        animator.SetBool("CanPlay", true);
+        mainMenuButtons.SetActive(true);
     }
 }

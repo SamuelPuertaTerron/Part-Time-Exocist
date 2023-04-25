ee3bd3adf77a0d96c9c10baa70bd0a5bae3d4aad
Jay <91475458+JayK1tsune@users.noreply.github.com>
2023-04-25 16:24:40 +0100

    Added monster scripts

---
+++ Assets/Scripts/Sam/Ghost/FireMonster.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PartTimeExocist
+{
+    public class FireMonster : GhostAIBase
+    {
+        protected override string GetEnemyName() {
+            return "Mr Hothead";
+        }
+        protected override void OnTakeDamage()
+        {
+            transform.localScale -= new Vector3(0.1f, 0.1f, 0.0f);
+
+            transform.position = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0.0f);
+        }
+
+        protected override void OnDeath()
+        {
+            transform.localScale = Vector3.one;
+            Destroy(gameObject, 1.2f);
+        }
+    }
+}
